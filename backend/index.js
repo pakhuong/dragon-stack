ed5d1781a1e72414a3c1180c68e72560a7b33efe
Khuong Pham <anhkhuong1602@gmail.com>
2019-03-31 16:34:05 +0700

    Improved the Dragon class

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const Dragon = require('./dragon');
 
-const fooey = new Dragon(new Date(), 'fooey');
-const baloo = new Dragon(new Date(), 'baloo');
+const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
+const baloo = new Dragon({ birthdate: new Date(), nickname: 'baloo' });
+const mimar = new Dragon();
+
+setTimeout(() => {
+    const gooby = new Dragon();
+
+    console.log('gooby', gooby);
+}, 3000);
 
 console.log('fooey', fooey);
 console.log('baloo', baloo);
+console.log('mimar', mimar);
