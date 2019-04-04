73e9ba726ec0db90553cb9824117c72cf2d114df
Khuong Pham <anhkhuong1602@gmail.com>
2019-04-04 22:53:46 +0700

    Added traits and default values for traits when instantiating dragons

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
-const Dragon = require('./dragon');
+const Dragon = require("./dragon");
 
-const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
-const baloo = new Dragon({ birthdate: new Date(), nickname: 'baloo' });
+const fooey = new Dragon({ birthdate: new Date(), nickname: "fooey" });
+
+const baloo = new Dragon({
+  birthdate: new Date(),
+  nickname: "baloo",
+  traits: [{ traitType: "backgroundColor", traitValue: "green" }]
+});
+
 const mimar = new Dragon();
 
 setTimeout(() => {
-    const gooby = new Dragon();
+  const gooby = new Dragon();
 
-    console.log('gooby', gooby);
+  console.log("gooby", gooby);
 }, 3000);
 
-console.log('fooey', fooey);
-console.log('baloo', baloo);
-console.log('mimar', mimar);
+console.log("fooey", fooey);
+console.log("baloo", baloo);
+console.log("mimar", mimar);
